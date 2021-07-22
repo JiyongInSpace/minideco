b1cbd9164edc86da28858b2efec84f1e59a80adb
JiyongInSpace <blaziken@naver.com>
2021-07-22 13:22:02 +0900

    v1.12
    test

---
+++ js/category.js
@@ -0,0 +1,49 @@
+// index.js
+
+function init(){
+    fetch('../data/category.json')
+    .then(res => res.json())
+    .then(data => callback(data));
+
+    function callback(data){
+        const categoryCons = document.querySelector(".category-item-cons");
+
+        data.item.forEach((items, index) => {
+            const div = document.createElement("div");
+            div.className = "category-item-con";
+            div.innerHTML =
+            `<figure class="category-item">
+                <figcaption>
+                    <img src="${items.image}" alt="image${index}">
+                    <div class="fig-bg">
+                        <div class="fig-bt1">
+                            <a href=""><div>
+                                <span class="material-icons-outlined">
+                                    open_in_new
+                                    </span></div></a>
+                            <a href=""><div>
+                                <span class="material-icons-outlined">
+                                    add_shopping_cart
+                                    </span></div></a>
+                            <a href=""><div>
+                                <span class="material-icons-outlined">
+                                favorite
+                                </span></div></a>
+                        </div>
+                    </div>                  
+                </figcaption>
+            </figure>
+            <div class="item-tag">
+                <div class="item-tag-price">
+                    <p>${items.name}</p>
+                    <p>${items.price}</p>
+                </div>
+                <div class="item-tag-content">
+                    ${items.detail}
+                </div>
+            </div>`;
+            categoryCons.appendChild(div);
+        })
+    }
+}
+window.addEventListener('load', init);
--- js/index.js
+++ js/index.js
@@ -35,12 +35,62 @@ showBtn.addEventListener("click", showSns)
 hiddenSns();
 
 
+// special json
+const specialList = document.querySelector(".special-list")
+
+function init(){
+    fetch('../data/index.json')
+    .then(res => res.json())
+    .then(data => callback(data));
+
+    function callback(data){
+        const specialList = document.querySelector(".special-list");
+
+        data.special.forEach((items, index) => {
+            const div = document.createElement("div");
+            div.className = `special-item-con`;
+            div.innerHTML =
+            `<figure class="special-item">
+                <img src="${items.image}" alt="special${index}">
+                <figcaption>
+                    <div class="fig-bg">
+                        <div class="fig-bt1">
+                            <a href=""><div>
+                                <span class="material-icons-outlined">
+                                    open_in_new
+                                    </span></div></a>
+                            <a href=""><div>
+                                <span class="material-icons-outlined">
+                                    add_shopping_cart
+                                    </span></div></a>
+                            <a href=""><div>
+                                <span class="material-icons-outlined">
+                                favorite
+                                </span></div></a>
+                        </div>
+                    </div>                  
+                </figcaption>
+            </figure>
+            <div class="item-tag">
+                <div class="item-tag-price">
+                    <p>${items.name}</p>
+                    <p>${items.price}</p>
+                </div>
+                <div class="item-tag-content">
+                    ${items.detail}
+                </div>
+            </div>`;
+            specialList.appendChild(div);
+        })
+    }
+}
+window.addEventListener('load', init);
+
 // special slide
 const leftBtn = document.querySelector(".slide-left");
 const rightBtn = document.querySelector(".slide-right");
 const slideBox = document.querySelector(".special-slide")
-const specialList = document.querySelector(".special-list")
-const specialItem = document.querySelectorAll(".special-list .special-item-con")
+
 
 let slideNum = 0;
 
@@ -48,16 +98,15 @@ function oneSlide(){
     specialList.style.transform = `translateX(-${specialList.offsetWidth/8*slideNum}px)`;
 }
 function specialSlide(event){
+    const specialItem = document.querySelectorAll(".special-list .special-item-con")
+
     let slideEa = Math.round(8/(specialList.getBoundingClientRect().width/slideBox.getBoundingClientRect().width));
     if(event.target.innerText == "오른쪽" && slideNum < specialItem.length-slideEa ){
         slideNum++;
-        oneSlide();
-        console.log()
-        console.log()
     } else if(event.target.innerText == "왼쪽" && slideNum > 0){
         slideNum--;
-        oneSlide();
     }
+    oneSlide();
 }
 
 leftBtn.addEventListener("click", specialSlide)
